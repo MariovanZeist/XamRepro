be6f5bdafc5e241a6f06f320b7bdd09fad6a1f5d
Mario van Zeist <m.vanzeist@home.nl>
2020-03-17 17:36:08 +0100

    Initial commit

---
+++ Xam.Controls/Platforms/iOS/DummyRenderer.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+#if __MOBILE__
+using Xamarin.Forms.Platform.iOS;
+using NativeLabel = UIKit.UILabel;
+#else
+using Xamarin.Forms.Platform.MacOS;
+using AppKit;
+using NativeLabel = AppKit.NSTextField;
+#endif
+[assembly: ExportRenderer(typeof(Label), typeof(FixXam.Forms.Platforms.iOS.DummyRenderer))]
+namespace FixXam.Forms.Platforms.iOS
+{
+	public class DummyRenderer : ViewRenderer<Label, NativeLabel>
+	{
+
+	}
+}
